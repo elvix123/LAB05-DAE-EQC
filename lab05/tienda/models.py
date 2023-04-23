6f9c70e4db7731f257922723231dd19fdeac421e
Elvis Quecara Cruz <112021979+elvix123@users.noreply.github.com>
2023-04-23 14:31:14 -0500

    Add files via upload

---
+++ lab05/tienda/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+# Create your models here.
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.nombre
+    
+class Producto(models.Model):
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=200)
+    precio = models.DecimalField(max_digits=6, decimal_places=2)
+    stock = models.IntegerField(default=0)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.nombre
+    
+class Cliente(models.Model):
+    nombres = models.CharField(max_length=200)
+    apellidos = models.CharField(max_length=200)
+    dni = models.CharField(max_length=10)
+    telefono = models.CharField(max_length=12)
+    direccion = models.CharField(max_length=200)
+    email = models.EmailField()
+    fecha_nacimiento = models.DateField('Fecha de Nacimiento')
+    fecha_publicacion = models.DateField('Fecha Publicacion')
+
+    def __str__(self):
+        return self.nombres
